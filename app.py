e5ae12be9f0e0d5dd3c73a95bced41a4f4c05683
iamabdurrk <abdurrahmank2002@gmail.com>
2025-02-05 18:29:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,14 @@
-app.py
+def fibonacci(n):
+    if n <= 0:
+        return "Input must be a positive integer."
+    elif n == 1:
+        return [0]
+    elif n == 2:
+        return [0, 1]
+    
+    sequence = fibonacci(n - 1)
+    sequence.append(sequence[-1] + sequence[-2])
+    return sequence
+
+n_terms = int(input("Enter the number of terms: "))
+print(fibonacci(n_terms))
